611ed1eb5788dd643bdc7768b4240f2c8ad8a06a
aki <206490839+noenii@users.noreply.github.com>
2025-10-10 14:48:09 +0630

    added files from learning python

---
+++ code/learning/2.py
@@ -0,0 +1,9 @@
+print("Do stuff with functions?")
+print("Print = Function")
+print("Functions are how you do stuff.")
+print("And the stuff in the parentheses is what it's using.")
+#Comments are used to annotate and explain.
+"""
+Multi-line commments too.
+"""
+#   Becareful with indentations.
---
+++ code/learning/3.py
@@ -0,0 +1,9 @@
+x = "Variables are very useful"
+print(x)
+#Becareful with variable names. Cannot start with numbers, can't have hyphens and shouldn't use other python keywords (e.g. class, if, and, or).
+"""
+Variables are assigned with the equal operator (=).
+There are many ways to assign variables.
+Variables can also be changed.
+del deletes a variable.
+"""
