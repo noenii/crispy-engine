c1723ea916780f4d535c468f6c981512ff7af00c
aki <206490839+noenii@users.noreply.github.com>
2025-10-12 09:42:21 +0630

    10/12/2025.1

---
+++ code/learning/8.py
@@ -0,0 +1,8 @@
+for i in range(11):
+    print(i)
+#can also iterate through lists
+while True:
+    print("keanu reeves is the goat")
+    break
+#can also nest loops
+#continue skips an iteration, break exits a loop, pass for empty loops
---
+++ code/learning/9.py
@@ -0,0 +1,14 @@
+#You can make your own functions too, there are many reasons for doing so such as:
+#Reusability, Modularity, Readability, Maintainability
+
+def add(num1=1, num2=1):
+    #code that will be executed
+    return num1 + num2
+
+print(add(1, 1))
+
+#you can also specify the parameters so order doesnt better
+#can also nest functions
+#theres also lambda functions which are basically a shorter way to define functions
+#functions also call themselves for recursion
+#pass is as a placeholder for functions but also loops and classes
