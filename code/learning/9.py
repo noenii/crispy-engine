a148582bc8cbcede693e82ee18ef404c2b16a8ae
aki <206490839+noenii@users.noreply.github.com>
2025-10-13 05:32:35 +0900

    10/12/25.2

--- code/learning/9.py
+++ code/learning/9.py
@@ -9,6 +9,20 @@ print(add(1, 1))
 
 #you can also specify the parameters so order doesnt better
 #can also nest functions
+
 #theres also lambda functions which are basically a shorter way to define functions
+
 #functions also call themselves for recursion
-#pass is as a placeholder for functions but also loops and classes
+
+#pass is as a placeholder for functions but also loops and classes to prevent errors
+
+#local                      vs                  global
+#declared outside functions             declared inside functions
+
+#created when the program starts        created when the function is called
+#and destroyed when it ends             and destroyed when it returns
+
+#shared across all functions            not shared, exists only in its function
+
+#can be accessed throught the           can only be accessed in the function
+#entire program
