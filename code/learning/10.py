f0de0beca77156eadb5403df4b7fc452b42211d4
aki <206490839+noenii@users.noreply.github.com>
2025-10-13 09:59:37 +0630

    10/13/25.1

--- code/learning/10.py
+++ code/learning/10.py
@@ -1,5 +1,5 @@
 #fibonacci sequence calculator
-
+'''
 def calculate(iterations):
     if iterations == 0:
         return 0
@@ -9,3 +9,10 @@ def calculate(iterations):
         return calculate(iterations - 1) + calculate(iterations - 2)
     
 print(calculate(100))
+'''
+#*args to accept an arbitrary number of parameters (unordered)
+#**kwargs for the same thing but ordered, eg: function(a=1, b=2, c=3)
+def dosmth(*args):
+    return sum(args)
+print(dosmth(9, 10))
+#learning about class stuff but im gonna write about it later
