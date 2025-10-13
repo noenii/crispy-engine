45b149a5b84748f35cd876a88c960f19119254c4
aki <206490839+noenii@users.noreply.github.com>
2025-10-13 21:29:37 +0900

    10/13/25.1

---
+++ code/learning/11.py
@@ -0,0 +1,14 @@
+#functions are first class objects, which means they can be treated like variables
+#you can assign functions to variables
+#pass them as arguments to other functions
+#return them from functions
+#store them in data structures such as lists or dictionaries
+
+#map() is used to apply a function to every item in an iterable
+#or smth like that (list, tuple, set, dicts...)
+
+#reduce() from functools to apply functions to reduce iterables
+
+#filter() is a function to filter iterables with another function
+
+#nesting functions r cool
