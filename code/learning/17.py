214677355d5106170a5a1e2460f867ae04a3eddb
aki <206490839+noenii@users.noreply.github.com>
2025-10-23 08:01:54 +0900

    10/22/25.2

--- code/learning/17.py
+++ code/learning/17.py
@@ -8,7 +8,9 @@ class person:
         self.name = name
         self.age = age
 
-person1 = person("me", 15)
+#a class is like a blueprint, and the object is the actual building
+
+person1 = person("aki", 15) #this is an object
 
 print(person1.name, person1.age, person1.species)
 
@@ -19,3 +21,19 @@ print(person1.name, person1.age, person1.species)
 person2 = person("haru", 16)
 
 print(person2.name, person2.age, person2.species)
+
+#inheritance is when classes "inherit" from other classes
+#code below does not work
+'''
+class friendly:
+    def __init__(self, relation):
+        self.relation = relation
+
+class friend(person, friendly):
+    def info(self):
+        print(self.name, self.age, self.relation)
+
+friend1 = friend("vib", 17)
+
+print(friend1.relation())
+'''
