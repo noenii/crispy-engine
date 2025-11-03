3b123f99ac6adaae5a388dc9e4c12b4842c06013
aki <206490839+noenii@users.noreply.github.com>
2025-11-03 14:58:39 +0630

    11/3/25.3

--- code/bpy/cube.py
+++ code/bpy/cube.py
@@ -1,1 +1,4 @@
 import bpy  #this must be run in blender! if you dont have blender, bpy must be manually installed
+
+bpy.ops.mesh.primitive_cube_add(size=2, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+print("Spawned a cube!")
