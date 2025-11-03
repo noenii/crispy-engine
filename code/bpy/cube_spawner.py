c7203404f28d9ec9618eb7274f2b8c48a20929cc
aki <206490839+noenii@users.noreply.github.com>
2025-11-04 02:26:33 +0900

    11/3/25.2

---
+++ code/bpy/cube_spawner.py
@@ -0,0 +1,19 @@
+import bpy
+
+class AddCube(bpy.types.Operator):
+    bl_idname = "object.add_cube"
+    bl_label = "Add Cube"
+    
+    def execute(self, context):
+        bpy.ops.mesh.primitive_cube_add(enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+        self.report({'INFO'}, "Spawned a cube!")
+        return {'FINISHED'}
+    
+def register():
+    bpy.utils.register_class(AddCube)
+
+def unregister():
+    bpy.utils.unregister_class(AddCube)
+
+if __name__ == "__main__":
+    register()
