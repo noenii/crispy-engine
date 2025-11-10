d6117e8193293e0cb2b0f0b29593b8ec3162c5ce
aki <206490839+noenii@users.noreply.github.com>
2025-11-10 22:00:17 +0900

    11/10/25.1

---
+++ code/c/learning/2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("This prints something into the console.\n\n");
+    printf("Not as casual as python.\nDouble quotes for str, and single quotes for a single char.\n\n");
+    printf("Line breaks must be added manually too.\n\n");
+    return 0;
+}
---
+++ code/c/learning/3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    //this is a single line comment
+    /*
+    THIS IS A MULTI
+    LINE
+    COMMENT
+    */
+    return 0;
+}
