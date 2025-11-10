bea11b316eb1fb8894d1b3908a8e82a69611e652
aki <206490839+noenii@users.noreply.github.com>
2025-11-10 10:30:17 +0630

    11/10/25.1

---
+++ code/c/learning/1.c
@@ -0,0 +1,14 @@
+//default c file
+#include <stdio.h>  //include is basically import from python
+
+//c ignores empty lines
+
+int main () {   //a function
+
+    //code in here will be executed
+
+    return 0;   //return 0 ends the main function (also return 0 is basically 200 OK :> any other number signals an error)
+
+}   //end of main()
+
+//c syntax: semicolons to end a statement
