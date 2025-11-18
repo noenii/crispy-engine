f417f1540a959c9ac72fda1f914462c78d835546
aki <206490839+noenii@users.noreply.github.com>
2025-11-18 12:55:54 +0630

    11/18/25.2

---
+++ code/c/learning/calc.c
@@ -0,0 +1,12 @@
+#include "calc.h"
+//for 18.c
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int subtract(int x, int y) {
+    return x - y;
+}
+
+//this file defines the functions in calc.h
---
+++ code/c/learning/calc.h
@@ -0,0 +1,8 @@
+#ifndef CALC_H      //dont think about it too much, this just make sure including it twice wont crash the program
+#define CALC_H
+//for 18.c
+
+int add(int x, int y);
+int subtract(int x, int y);
+
+#endif
