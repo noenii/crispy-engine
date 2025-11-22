e5ee3b53cf14846d7d68e74adecb6702b00dd270
aki <206490839+noenii@users.noreply.github.com>
2025-11-22 09:30:34 +0630

    11/22/25.1

--- code/c/arraa.c
+++ code/c/arraa.c
@@ -5,67 +5,68 @@
 int main() {
     int option;
     bool running = true;
-    printf("pArrs! DYNAMIC ARRAYS!!!!!\n");
 
     int count = 0;
     int *pArr = NULL;
 
-    while(running) {
+    printf("pArrs! DYNAMIC ARRAYS!!!!!\n");
+
+    while (running) {
         printf("Select: 1. View | 2. View all | 3. Insert | 4. Append | 5. Remove | 6. Clear | 7. Exit\n");
         scanf("%d", &option);
         getchar();
         printf("\n");
         switch(option) {
             case 1: {
-                int o1;
-                if(count == 0) {printf("pArr is empty.\n"); break;}
+                int i;
+                if (count == 0) { printf("Empty.\n"); break; }
                 printf("Enter an index: ");
-                scanf("%d", &o1);
+                scanf("%d", &i);
                 getchar();
-                printf("\n");
-                if(o1 >= count + 1 || o1 <= -1) {printf("Error: Out of bounds.\n"); break;}
-                printf("%d\n", pArr[o1]);
+                if (i < 0 || i >= count) {
+                    printf("Error: Out of bounds.\n");
+                    break;
+                }
+                printf("%d\n", pArr[i]);
                 break;
             }
             case 2: {
-                if(count == 0) {printf("pArr is empty.\n"); break;}
-                for(int i = 0; i < count; i++) {
+                if (count == 0) { printf("Empty.\n"); break; }
+                for (int i = 0; i < count; i++)
                     printf("%d ", pArr[i]);
-                }
                 printf("End\n");
                 break;
             }
             case 3: {
-                int num;
-                int pos;
+                int num, pos;
                 printf("Value: ");
                 scanf("%d", &num);
-                getchar();
                 printf("Index: ");
                 scanf("%d", &pos);
                 getchar();
-                if(pos >= count + 1) {printf("go append instead cus im lazy =P\n"); break;}
-                count++;
-                int *dummy = realloc(pArr, sizeof(*pArr) * count);
-                if(!dummy) {printf("Memory allocation failed.\n"); return EXIT_FAILURE;}
-                pArr = dummy;
-                for(int i = count; i >= 0; i--) {
-                    pArr[i] = pArr[i - 1];
-                    if(i == pos) {pArr[i] = num; break;}
+                if (pos < 0 || pos > count) {
+                    printf("Invalid index.\n");
+                    break;
                 }
+                int *dummy = realloc(pArr, sizeof(*pArr) * (count + 1));
+                if (!dummy) {printf("Memory allocation failed.\n"); return EXIT_FAILURE;}
+                pArr = dummy;
+                for (int i = count; i > pos; i--)
+                    pArr[i] = pArr[i - 1];
+                pArr[pos] = num;
+                count++;
                 break;
-
             }
             case 4: {
                 int num;
                 printf("Value: ");
                 scanf("%d", &num);
                 getchar();
-                count++;
-                int *dummy = realloc(pArr, sizeof(*pArr) * count);
-                if(!dummy) {printf("Memory allocation failed.\n"); return EXIT_FAILURE;}
+                int *dummy = realloc(pArr, sizeof(*pArr) * (count + 1));
+                if (!dummy) {printf("Memory allocation failed.\n"); return EXIT_FAILURE;}
                 pArr = dummy;
                 pArr[count] = num;
+                count++;
                 break;
             }
             case 5: {
@@ -73,34 +74,41 @@ int main() {
                 printf("Index: ");
                 scanf("%d", &pos);
                 getchar();
-                for(int i = count; i >= pos; i--) {
-                    pArr[i - 1] = pArr[i];
-                    if(i-1 == pos) {break;}
+                if (pos < 0 || pos >= count) {
+                    printf("Invalid index.\n");
+                    break;
                 }
+                for (int i = pos; i < count - 1; i++)
+                    pArr[i] = pArr[i + 1];
                 count--;
-                int *dummy = realloc(pArr, sizeof(*pArr) * count);
-                if(!dummy) {printf("Memory allocation failed.\n"); return EXIT_FAILURE;}
-                pArr = dummy;
+                if (count == 0) {
+                    free(pArr);
+                    pArr = NULL;
+                } else {
+                    int *dummy = realloc(pArr, sizeof(*pArr) * count);
+                    if (!dummy) {printf("Memory allocation failed.\n"); return EXIT_FAILURE;}
+                    pArr = dummy;
+                }
                 break;
             }
             case 6: {
-                printf("Deallocating...\n");
                 free(pArr);
                 pArr = NULL;
-                printf("Deallocation successful.\n");
+                count = 0;
+                printf("Cleared.\n");
                 break;
             }
             case 7: {
-                printf("Deallocating...\n");
+                printf("Exiting...\n");
                 free(pArr);
-                pArr = NULL;
-                printf("Deallocation successful.\n");
-                return EXIT_SUCCESS;
+                printf("Exit successfull.\n");
+                return 0;
             }
-            default: {
+            default:
+                printf("Invalid option.\n");
                 break;
-            }
         }
+        printf("\n");
     }
     return 0;
 }
